a5eb713c24301574f1844c1f3431703e2753925e
jackal7819 <jackal3166@gmail.com>
2025-07-23 10:47:37 +0300

    refactor: convert CardList to functional component

--- rs-react-app/src/components/CardList.tsx
+++ rs-react-app/src/components/CardList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Character } from '../types';
 import { Card } from './Card';
 
@@ -6,23 +5,21 @@ interface CardListProps {
   data: Character[];
 }
 
-export class CardList extends React.Component<CardListProps> {
-  render() {
-    if (!this.props.data.length) {
-      return <p className="p-4 text-gray-500">No results</p>;
-    }
-
-    return (
-      <div className="grid gap-4 py-4">
-        {this.props.data.map((item) => (
-          <Card
-            key={item.id}
-            name={item.name}
-            description={`${item.status} - ${item.species}`}
-            image={item.image}
-          />
-        ))}
-      </div>
-    );
+export const CardList = ({ data }: CardListProps) => {
+  if (!data.length) {
+    return <p className="p-4 text-gray-500">No results</p>;
   }
-}
+
+  return (
+    <div className="grid gap-4 py-4">
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          name={item.name}
+          description={`${item.status} - ${item.species}`}
+          image={item.image}
+        />
+      ))}
+    </div>
+  );
+};
