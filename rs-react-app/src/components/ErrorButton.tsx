bcf5b36f6aac641b02f786f5c2ec82071186e817
jackal7819 <jackal3166@gmail.com>
2025-07-23 11:15:47 +0300

    refactor: convert ErrorButton from class to function

--- rs-react-app/src/components/ErrorButton.tsx
+++ rs-react-app/src/components/ErrorButton.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 
-export class ErrorButton extends React.Component {
-  state = { throw: false };
+export const ErrorButton = () => {
+  const [shouldThrow, setShouldThrow] = useState(false);
 
-  render() {
-    if (this.state.throw) {
-      throw new Error('Checking ErrorBoundary');
-    }
-    return (
-      <button
-        type="button"
-        className="bg-rose-500 font-medium rounded-lg text-xl px-5 py-2.5 text-center flex items-center text-black hover:bg-rose-600 duration-500 cursor-pointer w-full justify-center"
-        onClick={() => this.setState({ throw: true })}
-      >
-        Throw Error
-      </button>
-    );
+  if (shouldThrow) {
+    throw new Error('Checking ErrorBoundary');
   }
-}
+
+  return (
+    <button
+      type="button"
+      className="bg-rose-500 font-medium rounded-lg text-xl px-5 py-2.5 text-center flex items-center text-black hover:bg-rose-600 duration-500 cursor-pointer w-full justify-center"
+      onClick={() => setShouldThrow(true)}
+    >
+      Throw Error
+    </button>
+  );
+};
