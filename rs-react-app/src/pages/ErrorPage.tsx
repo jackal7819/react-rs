3547eadb238fbcbcbcb414d4d8c92560f21a78a7
jackal7819 <jackal3166@gmail.com>
2025-07-23 17:58:37 +0300

    feat: add navbar component with navigation links

--- rs-react-app/src/pages/ErrorPage.tsx
+++ rs-react-app/src/pages/ErrorPage.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
 
 export const ErrorPage = () => (
-  <main className="grid min-h-screen px-8 antialiased text-slate-400 bg-slate-900 place-items-center text-primary">
+  <main className="grid min-h-screen px-8 antialiased text-slate-400 bg-slate-900 place-items-center">
     <div className="text-center">
       <p className="font-semibold text-9xl">404</p>
       <h1 className="mt-4 text-3xl font-bold tracking-tight capitalize sm:text-5xl">
