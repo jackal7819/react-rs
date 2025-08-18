2763827f9b7ab5293502b6f305bd8bb632ab34db
jackal7819 <jackal3166@gmail.com>
2025-08-18 16:14:29 +0300

    feat: create Zustand store for form entries

---
+++ forms/src/store/useFormStore.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand';
+
+export interface FormData {
+	name: string;
+	age: number;
+	email: string;
+	gender?: 'male' | 'female';
+	avatar?: string;
+	country?: string;
+}
+
+interface FormStore {
+	submittedData: FormData[];
+	addData: (data: FormData) => void;
+}
+
+export const useFormStore = create<FormStore>((set) => ({
+	submittedData: [],
+	addData: (data) => set((state) => ({ submittedData: [...state.submittedData, data] })),
+}));
