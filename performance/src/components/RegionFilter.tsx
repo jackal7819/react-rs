ba0cc330fc7e2aa36c239848ffd2da68086ba311
jackal7819 <jackal3166@gmail.com>
2025-08-26 18:03:27 +0300

    feat: add RegionFilter component for filtering countries by region

---
+++ performance/src/components/RegionFilter.tsx
@@ -0,0 +1,25 @@
+interface RegionFilterProps {
+	regions: string[];
+	selectedRegion: string;
+	onChange: (region: string) => void;
+}
+
+const RegionFilter: React.FC<RegionFilterProps> = ({ regions, selectedRegion, onChange }) => (
+	<select
+		name='region'
+		id='region'
+		aria-label='Select Region'
+		value={selectedRegion}
+		onChange={(e) => onChange(e.target.value)}
+		className='p-2 text-white rounded bg-slate-700'
+	>
+		<option value=''>Select Region</option>
+		{regions.map((region) => (
+			<option key={region} value={region}>
+				{region}
+			</option>
+		))}
+	</select>
+);
+
+export default RegionFilter;
